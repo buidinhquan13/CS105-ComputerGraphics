d0b241d4fdb553391efeaa3dbac3dc5fb0d0205b
bdq-benjamin <buiquan13032003@gmail.com>
2024-06-03 22:42:44 +0700

    Updated

--- src/Experience/Utils/assets.ts
+++ src/Experience/Utils/assets.ts
@@ -23,20 +23,5 @@ export default [
 	   name: "living room",
 	   type: "glbModel",
 	   path: "/models/living room.glb"
-	},
-	// {
-	//    name: "room_01",
-	//    type: "glbModel",
-	//    path: "/models/room_01.glb"
-	// },
-	// {
-	//    name: "room_02",
-	//    type: "glbModel",
-	//    path: "/models/room_02.glb"
-	// },
-	// {
-	//     name: "screen",
-	//     type: "videoTexture",
-	//     path: "/textures/video.mp4"
-	// }
+	}
 ];
