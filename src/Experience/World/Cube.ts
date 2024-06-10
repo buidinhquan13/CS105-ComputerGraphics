0bfb27f1e0c3e8eba45efc3853079f69e151c612
bdq-benjamin <buiquan13032003@gmail.com>
2024-06-10 17:57:48 +0700

    update texture

--- src/Experience/World/Cube.ts
+++ src/Experience/World/Cube.ts
@@ -1,3 +1,82 @@
+// import * as THREE from "three";
+// import Experience from "../Experience";
+// import GSAP from "gsap";
+// import { GLTF } from "three/examples/jsm/loaders/GLTFLoader.js";
+// import Resources from "../Utils/Resources";
+
+// type LerpType = {
+//    current: number;
+//    target: number;
+//    ease: number;
+// };
+
+// export default class Cube {
+//    experience: Experience;
+//    scene: THREE.Scene;
+//    resources: Resources;
+//    lerp: LerpType;
+//    rotation: number;
+//    rectLight!: THREE.RectAreaLight;
+//    cubeRoom!: THREE.Object3D<THREE.Event>;
+//    cubeScene: THREE.Group;
+//    //for animations
+//    // mixer!: THREE.AnimationMixer;
+//    // swim!: THREE.AnimationAction;
+
+//    constructor() {
+//       this.experience = new Experience();
+//       this.scene = this.experience.scene;
+//       this.resources = this.experience.resources;
+//       this.cubeScene = (this.resources.items['cube'] as GLTF).scene;
+//       this.rotation = 0;
+//       //TODO: need to understand the moving camera along curves
+//       this.lerp = {
+//          current: 0,
+//          target: 0,
+//          ease: 0.1,
+//       };
+
+//       this.setModel();
+//    }
+
+//    setModel() {
+//       this.cubeScene.children.forEach((child: THREE.Object3D<THREE.Event>) => {
+//          child.castShadow = true;
+//          child.receiveShadow = true;
+
+//          //Threejs automatically group mesh together
+//          //So need to check if child is a group to add castShadow
+//          if (child instanceof THREE.Group) {
+//             child.children.forEach((groupChild) => {
+//                groupChild.castShadow = true;
+//                groupChild.receiveShadow = true;
+//             });
+//          }
+
+//          // child.scale.set(0, 0, 0);
+//          if (child.name === "Cube") {
+//             child.position.set(0, -1.5, 0);
+//             child.rotation.y = Math.PI / 4;
+//             this.cubeRoom = child;
+//          }
+
+//       });
+//       this.scene.add(this.cubeScene);
+//       this.cubeScene.scale.set(0.11, 0.11, 0.11);
+//    }
+
+//    resize() { }
+
+//    update() {
+//       this.lerp.current = GSAP.utils.interpolate(
+//          this.lerp.current,
+//          this.lerp.target,
+//          this.lerp.ease
+//       );
+//       this.cubeScene.rotation.y = this.lerp.current;
+//    }
+// }
+
 import * as THREE from "three";
 import Experience from "../Experience";
 import GSAP from "gsap";
@@ -19,9 +98,9 @@ export default class Cube {
    rectLight!: THREE.RectAreaLight;
    cubeRoom!: THREE.Object3D<THREE.Event>;
    cubeScene: THREE.Group;
-   //for animations
-   // mixer!: THREE.AnimationMixer;
-   // swim!: THREE.AnimationAction;
+   textureLoader: THREE.TextureLoader;
+   cubeTexture!: THREE.Texture;
+   cubeMaterial!: THREE.MeshStandardMaterial;
 
    constructor() {
       this.experience = new Experience();
@@ -36,20 +115,33 @@ export default class Cube {
          ease: 0.1,
       };
 
+      this.textureLoader = new THREE.TextureLoader();
+      this.loadTexture();
       this.setModel();
+   }
+
+   loadTexture() {
+      this.cubeTexture = this.textureLoader.load('assets/gen_checkerboard-1.png');
+      this.cubeMaterial = new THREE.MeshStandardMaterial({ map: this.cubeTexture });
    }
 
    setModel() {
       this.cubeScene.children.forEach((child: THREE.Object3D<THREE.Event>) => {
-         child.castShadow = true;
-         child.receiveShadow = true;
+         if (child instanceof THREE.Mesh) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+            child.material = this.cubeMaterial;
+         }
 
          //Threejs automatically group mesh together
          //So need to check if child is a group to add castShadow
          if (child instanceof THREE.Group) {
             child.children.forEach((groupChild) => {
-               groupChild.castShadow = true;
-               groupChild.receiveShadow = true;
+               if (groupChild instanceof THREE.Mesh) {
+                  groupChild.castShadow = true;
+                  groupChild.receiveShadow = true;
+                  groupChild.material = this.cubeMaterial;
+               }
             });
          }
 
